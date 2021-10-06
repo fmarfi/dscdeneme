d4af74f43f240195694a10ebc231242fe3a8127e
faertan <ilhanasimfurkan@gmail.com>
2021-10-06 16:12:00 +0300

    initial commit

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>RCIMUN-Home</title>
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <?php include 'topbar.php';?>
+        <br>
+        <h1 class="header01">RCIMUN</h1>
+
+        
+    </body>
+</html>
---
+++ topbar.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+    <nav class="topnav">
+        <a href="#">Home</a>   
+        <a href="#">Contact</a>
+
+    </div>
+
+    </body>
+</html>
